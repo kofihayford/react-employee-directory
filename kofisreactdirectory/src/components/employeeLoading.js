1a1b9a7b7f86b16c4b338832305d96b3a7ed3664
kofithebass <kofi@kofihayford.com>
2021-04-23 22:45:59 -0400

    Update employeeLoading.js
    randomuser api is pulling the data that I need now. had to decide between pulling specific details onyl and being limited to one record at a time or pulling the number of records I wanted and then picking out what I needed from those records. Opted for the second. Easy to use dot notation to grab the appropriate on as needed this way.

--- kofisreactdirectory/src/components/employeeLoading.js
+++ kofisreactdirectory/src/components/employeeLoading.js
@@ -6,32 +6,39 @@ import React, { useState, useEffect } from 'react'
 
 
 export default function EmployeeLoading() {
-    let URL = "https://randomuser.me/api/?results=50"
+    const [users, setUsers] = useState([])
+    let URL = "https://randomuser.me/api/?results=10"
     useEffect(() => {
         fetch(URL)
-            .then(res => res.json())
+            .then(res => {
+                return res.json()
+            })
+            .then(results => setUsers(results.results))
             .catch(e => console.log(e))
-    })
+    }, [])
+    console.log(users, 'this is our state users')
     return (
         <div>
-            <p className="name">
+            <p className="name"> Name:
             </p>
 
-            <p className="email">
+            <p className="email"> Email:
             </p>
 
-            <p className="phone">
+            <p className="phone"> Phone:
             </p>
 
-            <p className="age">
+            <p className="age"> Age:
             </p>
 
-            <p className="id">
+            <p className="id"> ID Number:
             </p>
 
-            <p className="dob">
+            <p className="dob"> Date of Birth:
             </p>
 
+            <p className="name"> Gender:
+            </p>
 
             <img src="" className="image" alt="avatarimg" />
         </div>
